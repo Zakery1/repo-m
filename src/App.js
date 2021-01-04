09bb1e54f2d42bbcd4e2c34583ab93849e96adc3
Zachary Graham <zakgraham@gmail.com>
2021-01-04 11:49:47 -0700

    add airbnb

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Jana's Website</div>;
+  return <div className="App">Jana's Website <a href="https://www.airbnb.com/rooms/plus/30375895?source_impression_id=p3_1609097004_K5g5rTZab7fMZw2l"><br/>AirBNB</a></div>;
 }
 
 export default App;
